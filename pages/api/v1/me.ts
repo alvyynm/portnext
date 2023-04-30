e8689311faa9eb4aeb97e94812e3fb17b88b4d1c
alvyynm <alvyynm@gmail.com>
2023-04-30 06:02:13 +0300

    define built-in types for /api/v1/me route

--- pages/api/v1/me.ts
+++ pages/api/v1/me.ts
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json({
     data: {
       me: true
